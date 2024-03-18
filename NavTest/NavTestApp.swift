d73b597fbb01628296a3d46ada659e27ea7b29b4
Om Shah <omdshah2004@gmail.com>
2024-03-18 14:31:01 -0500

    Initial Commit

---
+++ NavTest/NavTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavTestApp.swift
+//  NavTest
+//
+//  Created by Shah, Om on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NavTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
